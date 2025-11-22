1ec09350cb09909145b55bb6cb0d384978025abc
MikeZeDev <244155418+MikeZeDev2@users.noreply.github.com>
2025-11-22 09:43:55 +0100

    DayComics : improve manga regex

--- web/src/engine/websites/DayComics.ts
+++ web/src/engine/websites/DayComics.ts
@@ -99,7 +99,7 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override ValidateMangaURL(url: string): boolean {
-        return new RegExpSafe(`^${this.URI.origin}/content/\\d+$`).test(url);
+        return new RegExpSafe(`^${this.URI.origin}/(content|comic)/\\d+$`).test(url);
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
